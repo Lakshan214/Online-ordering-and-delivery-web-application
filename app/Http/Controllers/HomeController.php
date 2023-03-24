5045b26cea6cba138c2af2495701c70c3f32db83
Lakshan Rajapaksha <lrajapaksha58@gmail.com>
2023-03-24 22:50:08 +0530

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function redirect()
+    {
+      $userType=Auth::user()->userType;
+
+      if($userType=='1')
+       {
+             return view('admin.home');
+       }
+       elseif ($userType=='2') 
+        {
+             return view('deliver.home');
+       }
+       else
+      {
+             return view('dashboard');
+      }
+    } 
+
+    public function index()
+    {  
+      
+      return view('Home.index');
+    }
+}
