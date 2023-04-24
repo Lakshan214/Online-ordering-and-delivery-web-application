aa9f7abbf63c44d98eb1ea7ce1ba04751a418d6e
Lakshan Rajapaksha <lrajapaksha58@gmail.com>
2023-04-24 22:14:09 +0530

    add admin panal

---
+++ resources/views/admin/saidebar.blade.php
@@ -0,0 +1,36 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
+      <a class="sidebar-brand brand-logo" href="index.html"><img src="/admin/assets/images/logo.png"  style="height: 70px; width:200px;"  alt="logo" /></a>
+      <a class="sidebar-brand brand-logo-mini" href="index.html"><img src="/admin/assets/images/logo-mini.svg" alt="logo" /></a>
+    </div>
+    <ul class="nav">
+      <li class="nav-item profile">
+        <div class="profile-desc">
+          <div class="profile-pic">
+            <div class="count-indicator">
+              
+      <li class="nav-item menu-items">
+        <a class="nav-link" href="index.html">
+          <span class="menu-icon">
+            <i class="mdi mdi-speedometer"></i>
+          </span>
+          <span class="menu-title">Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item menu-items">
+        <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+          <span class="menu-icon">
+            <i class="mdi mdi-laptop"></i>
+          </span>
+          <span class="menu-title">Product</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-basic">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Product Add</a></li>
+          </ul>
+        </div>
+      </li>
+      
+    </ul>
+  </nav>
